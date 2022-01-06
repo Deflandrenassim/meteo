be3a7ed1012fb4b4952da5087ad0cf4ea3b836d6
ndeflandre <nassim.deflandre@niji.fr>
2022-01-06 11:35:03 +0100

    add html

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,40 @@
 <template>
- <div>
-   Bienvenue dans notre app Méteo 
+ <div> 
+   <div class="titre">
+  {{ hello }} dans notre app Méteo 
+   </div>
+
+   <div class="input-search">
+
+   <input type="search" name="search" id="search" placeholder="Recherche..">
+   </div>
+
+
+
  </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
- 
+  data (){
+    return{
+      hello: 'Hello World',
+      api_key: 'd4235acc5a46cf24a94dfe20005090ba'
+    }
+  }
   }
 
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style>
+body{
+  background-color: rgb(0, 179, 179);
+  font-size: 35px;
+}
+.titre{
+  text-align: center;
+}
 
 </style>
