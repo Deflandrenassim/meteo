a84c73eb2ae43478a8fed1b2f6107b146a882349
ndeflandre <nassim.deflandre@niji.fr>
2022-01-06 17:06:53 +0100

    fetch api openweather

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,48 +1,80 @@
 <template>
- <div> 
-   <div class="titre">
-  {{ hello }} dans notre app Méteo 
-   </div>
+  <div id="app">
+    <div class="titre"> {{ hello }} dans notre app Méteo</div>
+    <main>
 
-   <div class="input-search">
-   <input type="search" class="input" name="search" v-model="recherche" placeholder="Recherche..">
-   </div>
-  <div class="container">
-   <div class="date"> {{ recherche }} </div>
-   <div class="temperature"> date aujourdhui   </div>
-   <div class="temperature"> 20 degré  </div>
-   <div>  quel temps </div>
+    <div class="input-search">
+      <input
+        type="search"
+        class="input"
+        name="search"
+        v-model="recherche"
+        placeholder="Recherche.."
+        @keypress="fetchWeather"
+      />
+    </div>
+   
+<div class="container" v-if="typeof weather.main != 'undefined'">
+  <div class="geo">
+    {{ weather.name }}, {{ weather.sys.country}}
+    <div class="temp">
+    {{ Math.round(weather.main.temp)}}
+    </div>
 
+   
   </div>
-
-
-
+ <div class="temp">
  </div>
+</div>
+
+    </main>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data (){
-    return{
-      hello: 'Hello World',
-      api_key: 'd4235acc5a46cf24a94dfe20005090ba',
-      recherche: ''
-    }
-  }
-  }
-
+  name: "HelloWorld",
+  data() {
+    return {
+      api_key: "d4235acc5a46cf24a94dfe20005090ba",
+      base_url: "https://api.openweathermap.org/data/2.5/",
+      hello: "Hello World", // ma Data du titre
+      recherche: "",
+      weather: {},
+    };
+  },
+  methods: {
+    fetchWeather(evement) {
+      if (evement.key == "Enter") {
+        fetch(
+          `${this.base_url}weather?q=${this.recherche}&units=metric&APPID=${this.api_key}`
+        )
+          .then((res) => {
+            return res.json();
+          })
+          .then(this.setResults);
+      }
+    },
+    setResults(results) {
+      this.weather = results;
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-body{
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  justify-items: center;
+}
+body {
   background-color: rgb(0, 179, 179);
   font-size: 35px;
 }
-.titre{
+.titre {
   text-align: center;
 }
-
-
 </style>
